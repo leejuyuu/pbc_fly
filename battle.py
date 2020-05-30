27639e90e09757f6e95450ae97860f47e3efcfdf
Tzu-Yu Lee <leejuyuu@gmail.com>
2020-05-30 15:42:08 +0800

    Init the Plane object by loading image
    Load the image and draw a plane on the scene. The image loading function
    was borrowed from the chimp example.

--- battle.py
+++ battle.py
@@ -1,11 +1,33 @@
 """
 This module handles all battle mechanisms.
 """
+from pathlib import Path
 import pygame
 
+IMG_DIR = Path(__file__).resolve().parent / 'img'
+print(__file__)
+print(IMG_DIR)
+# functions to create our resources
+def load_image(name, colorkey=None):
+    fullname = IMG_DIR / name
+    try:
+        image = pygame.image.load(str(fullname))
+    except pygame.error:
+        print("Cannot load image:", fullname)
+        raise SystemExit(str(pygame.compat.geterror()))
+    image = image.convert()
+    if colorkey is not None:
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey, pygame.RLEACCEL)
+    return image, image.get_rect()
 
 class Plane(pygame.sprite.Sprite):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.image, self.rect = load_image('plane.png')
+
+
 
 
 
@@ -40,11 +62,11 @@ def main():
             # elif event.type == MOUSEBUTTONUP:
             #     fist.unpunch()
 
-            # allsprites.update()
+            allsprites.update()
 
             # Draw Everything
             screen.blit(background, (0, 0))
-            # allsprites.draw(screen)
+            allsprites.draw(screen)
             pygame.display.flip()
     pygame.quit()
 
