8a6edc10302b46be5d645c8fde09ade64d65f005
Tzu-Yu Lee <leejuyuu@gmail.com>
2020-06-14 22:02:56 +0800

    Fix bug: cannot quit program by clicking x button

--- main.py
+++ main.py
@@ -126,8 +126,10 @@ def main():
         # Event handling
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                pygame.quit()
                 return
             if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                pygame.quit()
                 return
 
         background1_rect.y += SCROLLING_SPEED
